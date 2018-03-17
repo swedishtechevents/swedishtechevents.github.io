f12ca4a5db0572f523b125c30b1529242d161817
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-03-17 19:37:03 +0100

    Add info about how events are fetched

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ class App extends Component {
         <Header title='Swedish Tech Events' subtitle='Events for developers, technologists, and other geeks in Sweden' />
         <main className='section'>
           <div className='container'>
-            <h2 className='title is-3'>About</h2>
-            <p>Lorem ipsum dolor sit amet, cu vel legere civibus delicata, ex dolores perfecto tincidunt usu. Pro ut natum iracundia, graeco utroque detraxit ad eos, mel id vocent diceret. Sonet saperet assueverit pro at, at ridens facilisis posidonium cum. Mei ex nostrum periculis. Eum prodesset vituperatoribus in, consulatu temporibus cu vis. <a href='https://github.com/swedishtechevents/events/issues/new' rel='noopener noreferrer' target='_blank'>Missing your event? Submit it!</a></p>
+            <p>Events are fetched from <a href='https://www.meetup.com/'>meetup.com</a> and from <a href='https://www.eventbrite.com/'>eventbrite.com</a> that is categorized with tech and from our GitHub. <a href='https://github.com/swedishtechevents/events/issues/new' rel='noopener noreferrer' target='_blank'>Missing your event? Submit it!</a></p>
             <br />
             <h2 className='title is-3'>Upcoming events</h2>
             <Events />
