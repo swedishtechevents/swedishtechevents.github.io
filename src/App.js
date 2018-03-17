e69f4dd2a27477414ada2e4ced111fbd5d021d87
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-03-17 20:15:53 +0100

    Remove from

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <Header title='Swedish Tech Events' subtitle='Events for developers, technologists, and other geeks in Sweden' />
         <main className='section'>
           <div className='container'>
-            <p>Events are fetched from <a href='https://www.meetup.com/'>meetup.com</a> and from <a href='https://www.eventbrite.com/'>eventbrite.com</a> that is categorized with tech and from our GitHub. <a href='https://github.com/swedishtechevents/events/issues/new' rel='noopener noreferrer' target='_blank'>Missing your event? Submit it!</a></p>
+            <p>Events are fetched from <a href='https://www.meetup.com/'>meetup.com</a> and <a href='https://www.eventbrite.com/'>eventbrite.com</a> that is categorized with tech and from our GitHub. <a href='https://github.com/swedishtechevents/events/issues/new' rel='noopener noreferrer' target='_blank'>Missing your event? Submit it!</a></p>
             <br />
             <Events />
           </div>
