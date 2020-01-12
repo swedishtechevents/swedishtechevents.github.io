71d8049e0d68491239f83457c5e9d9c75b17a163
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-01-13 00:01:01 +0100

    Remove history warning

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import 'whatwg-fetch';
 import 'bulma/css/bulma.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
-import createHistory from 'history/createBrowserHistory';
 import App from './App';
 
-const history = createHistory();
-
-ReactDOM.render(<App history={history} />, document.getElementById('root'));
+ReactDOM.render(<App history={require('history').createBrowserHistory()} />, document.getElementById('root'));
