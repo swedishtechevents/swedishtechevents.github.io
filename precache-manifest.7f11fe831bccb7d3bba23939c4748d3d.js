388bae256c8b33fd939f3a960092a77e1940ba68
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-01-12 22:59:09 +0100

    Updates

---
+++ precache-manifest.7f11fe831bccb7d3bba23939c4748d3d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc0ad02b8801f0fe69c604868ae43c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2834f3a179e673554465",
+    "url": "/static/css/2.23cfae6a.chunk.css"
+  },
+  {
+    "revision": "172b10f0ee4b3d5b5bb8",
+    "url": "/static/css/main.1ac436de.chunk.css"
+  },
+  {
+    "revision": "2834f3a179e673554465",
+    "url": "/static/js/2.dec9b836.chunk.js"
+  },
+  {
+    "revision": "a6aff573ce49ac878e4ab69d0a929897",
+    "url": "/static/js/2.dec9b836.chunk.js.LICENSE"
+  },
+  {
+    "revision": "172b10f0ee4b3d5b5bb8",
+    "url": "/static/js/main.4207bab8.chunk.js"
+  },
+  {
+    "revision": "919ac4a14c89f4e9301e",
+    "url": "/static/js/runtime-main.ddb9ed25.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
